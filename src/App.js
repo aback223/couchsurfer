c0701e55e9b93baababc13090fd9f82467f10b21
aback1227 <aback1227@gmail.com>
2018-04-20 00:30:53 -0400

    add home, signup, and login routes to router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './components/Homepage';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Homepage />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <Switch>
+            <Route exact path="/" component={Homepage}/>
+            <Route path="/signup" component={Signup}/>
+            <Route path="/login" component={Login}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
