7594e9cd92aaa74ba37b960912fb1602da00588f
aback1227 <aback1227@gmail.com>
2018-04-19 23:05:13 -0400

    import userReducer to index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import userReducer from './reducers/userReducer';
 
-const rootReducer = combineReducers({}); //import and add reducers
+const rootReducer = combineReducers({ userReducer }); //import and add reducers
 const store = createStore(
   rootReducer, 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
