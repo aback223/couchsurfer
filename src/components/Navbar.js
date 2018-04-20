9adfb25fad678cd775637b145aec32e450519d76
aback1227 <aback1227@gmail.com>
2018-04-20 03:06:05 -0400

    add a couch logo that links to homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import couch from '../couch.png'
 
 
 class Navbar extends Component {
   render() {
     return(
       <div className="navbar">
-        <NavLink to="/" className="navbar-left">Couchsurfer</NavLink>
+        <NavLink to="/" className="navbar-left"><img src={couch} className="couch-logo" alt="couch-logo"/></NavLink>
         <NavLink to="/login" className="navbar-right">Log In</NavLink>
         <NavLink to="/signup" className="navbar-right">Sign Up</NavLink>
       </div>
