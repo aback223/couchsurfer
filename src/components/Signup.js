b868429cc783b15ff2d22310b965982c91fedef0
aback1227 <aback1227@gmail.com>
2018-04-20 00:29:01 -0400

    create initial signup component

---
+++ src/components/Signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+  render() {
+    return(
+      <div>
+        This is the signup component.
+      </div>
+    )
+  }
+}
+
+export default Signup;
