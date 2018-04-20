096f3d09a4ba7597efec43b99b037f12ce25ae02
aback1227 <aback1227@gmail.com>
2018-04-20 02:34:02 -0400

    add padding to homepage, login, signup div

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Homepage extends Component {
   render() {
     return(
-      <div>
+      <div className="addPadding">
         This is the Homepage Component.
       </div>
     )
