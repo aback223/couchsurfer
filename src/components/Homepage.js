8fb27f8b61f5715fd6fbcd9e198a7e00328e69d6
aback1227 <aback1227@gmail.com>
2018-04-20 00:31:28 -0400

    moved Navbar to App component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar'
 
 class Homepage extends Component {
   render() {
     return(
       <div>
-        <Navbar/>
         This is the Homepage Component.
       </div>
     )
