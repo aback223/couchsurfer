b5076452f35c138d281d526688a34aa71c52d806
aback1227 <aback1227@gmail.com>
2018-04-19 23:08:53 -0400

    create Navbar component & import Navbar component to Homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar'
 
 class Homepage extends Component {
   render() {
     return(
       <div>
+        <Navbar/>
         This is the Homepage Component.
       </div>
     )
