904529b48653ce517a68a03c38e86b47463f69da
aback1227 <aback1227@gmail.com>
2018-04-21 00:04:52 -0400

    start building a signup form for homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,7 +8,29 @@ class Homepage extends Component {
       <div className="homeDiv"> 
         <HomeNav />
         <div className="addPadding homeContent">
-           <img src={couchworld} />
+          <div className="homeImage">
+            <img src={couchworld} />
+          </div>
+          <div className="homeSignup">
+            <form>
+              <div className="nameField">
+                <input type="text" name="firstName" placeholder="First Name" />
+                <input type="text" name="lastName" placeholder="Last Name" />
+              </div>
+              <div className="emailField">
+                <input type="text" name="email" placeholder="Email" />
+              </div>
+              <div className="passwordField">
+                <input type="password" name="password" placeholder="Password" />
+              </div>
+              <div className="locationField">
+                <input type="text" name="location" placeholder="City, State" />
+              </div>
+              <div className="signupButton">
+                <button>Sign Up</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     )
