47d81444dacae676a670b4ecd6aacb73774d9acb
aback1227 <aback1227@gmail.com>
2018-04-24 16:23:00 -0400

    export SignupForm component and import FormGroup from react-bootstrap

---
+++ src/components/SignupForm.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {FormGroup} from 'react-bootstrap';
+
+class SignupForm extends Component {
+  render() {
+    return(
+      <form>
+        <FormGroup>
+        </FormGroup>
+      </form>
+    )
+  }
+}
+
+export default SignupForm;
