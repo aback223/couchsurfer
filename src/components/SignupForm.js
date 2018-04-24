69b7f10f27e71e39e518993078ecfcf13e3d5b90
aback1227 <aback1227@gmail.com>
2018-04-24 16:38:32 -0400

    import FormControl to SignupForm

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {FormGroup} from 'react-bootstrap';
+import {FormGroup, FormControl} from 'react-bootstrap';
 
 class SignupForm extends Component {
   render() {
     return(
       <form>
         <FormGroup>
+          <FormControl type="text" />
         </FormGroup>
       </form>
     )
