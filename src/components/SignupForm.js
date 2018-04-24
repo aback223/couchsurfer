c399bfa3a9def82c2e28d78302707dac4ea1d30b
aback1227 <aback1227@gmail.com>
2018-04-24 19:52:26 -0400

    remove react-bootstrap import of Grid and Col in SignupForm

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,21 +1,37 @@
 import React, {Component} from 'react';
-import { Row, Col, Grid, FormGroup, FormControl } from 'react-bootstrap';
+import { Form, Row, Button, FormGroup, FormControl } from 'react-bootstrap';
 
 class SignupForm extends Component {
   render() {
     return(
-      <Grid>
-        <form>
+      <Form inline className="signupForm">
+        <Row>
+          <FormGroup>
+            <FormControl type="text" placeholder="First Name" />
+          </FormGroup>
+          <FormGroup>
+            <FormControl type="text" placeholder="Last Name" />
+          </FormGroup>
+        </Row>
+        <div className="singleRows">
           <Row>
             <FormGroup>
-              <FormControl type="text" placeholder="First Name" />
-            </FormGroup>
-            <FormGroup>
-              <FormControl type="text" placeholder="Last Name" />
+              <FormControl type="text" placeholder="Email" />
             </FormGroup>
           </Row>
-        </form>
-      </Grid>
+          <Row>
+            <FormGroup>
+              <FormControl type="password" placeholder="Password" />
+            </FormGroup>
+          </Row>
+          <Row>
+            <FormGroup>
+              <FormControl type="text" placeholder="City, State" />
+            </FormGroup>
+          </Row>
+        </div>
+        <button type="button" className="signupButton">Sign Up</button>
+      </Form>
     )
   }
 }
