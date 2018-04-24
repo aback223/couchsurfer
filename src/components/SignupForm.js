d2c70fe262d446dcb2a745dc04d55a7252740855
aback1227 <aback1227@gmail.com>
2018-04-24 16:44:26 -0400

    import Row and Col from react-boostrap to SignupForm

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
-import { Grid, FormGroup, FormControl } from 'react-bootstrap';
+import { Row, Col, Grid, FormGroup, FormControl } from 'react-bootstrap';
 
 class SignupForm extends Component {
   render() {
     return(
       <Grid>
         <form>
-          <FormGroup>
-            <FormControl type="text" placeholder="First Name" />
-          </FormGroup>
-          <FormGroup>
-            <FormControl type="text" placeholder="Last Name" />
-          </FormGroup>
+          <Row>
+            <FormGroup>
+              <FormControl type="text" placeholder="First Name" />
+            </FormGroup>
+            <FormGroup>
+              <FormControl type="text" placeholder="Last Name" />
+            </FormGroup>
+          </Row>
         </form>
       </Grid>
     )
