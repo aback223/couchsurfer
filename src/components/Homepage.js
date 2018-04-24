84a0ab1f4ff6829821990ae1f8dce35e6cbd0409
aback1227 <aback1227@gmail.com>
2018-04-24 19:18:47 -0400

    remove Row, Col, Grid import from Homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,30 +2,21 @@ import React, { Component } from 'react';
 import couchworld from '../couchworld.png';
 import HomeNav from './HomeNav';
 import SignupForm from './SignupForm';
-import { Row, Col, Grid } from 'react-bootstrap';
 
 class Homepage extends Component {
   render() {
     return(
-      <Grid>
-        <Row>
-          <HomeNav />
-        </Row>
-        <Row className="homeContent">
-          <div className="addPadding">
-            <Col sm={6}>
-              <div className="homeImage">
-                <img src={couchworld} alt="couchworld"/>
-              </div>
-            </Col>
-            <Col sm={6}>
-              <div className="homeSignup">
-                <SignupForm />
-              </div>
-            </Col>
+      <div className="homeGrid">
+        <HomeNav />
+        <div className="addPadding homeContent">
+          <div className="homeImage">
+            <img src={couchworld} alt="couchworld"/>
           </div>
-        </Row>
-      </Grid>
+          <div className="homeSignup">
+            <SignupForm />
+          </div>
+        </div>
+      </div>
     )
   }
 }
