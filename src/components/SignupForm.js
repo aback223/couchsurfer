e5b28bc08607fb4255224e3500b5531b2f5c920a
aback1227 <aback1227@gmail.com>
2018-04-24 22:24:36 -0400

    write handleChange and value prop to retrieve current state of signup form

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -10,42 +10,85 @@ class SignupForm extends Component {
       lastName: '', 
       email: '', 
       password: '', 
-
+      location: ''
     }
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  createNewAccount = (event) => {
+    event.preventDefault();
   }
 
   render() {
     return(
-      <Form inline className="signupForm">
-        <div className="nameFields">
-          <Row>
-            <FormGroup validationState={this.getValidationState()}>
-              <FormControl type="text" name="firstName" placeholder="First Name" />
-              <FormControl type="text" name="lastName" placeholder="Last Name" />
-            </FormGroup>
-          </Row>
-        </div>
-        <div className="singleRows">
-          <Row>
-            <FormGroup>
-              <FormControl type="text" name="email" placeholder="Email" />
-            </FormGroup>
-          </Row>
-          <Row>
-            <FormGroup>
-              <FormControl type="password" name="password" placeholder="Password" />
-            </FormGroup>
-          </Row>
-          <Row>
-            <FormGroup>
-              <FormControl type="text" name="location" placeholder="City, State" />
-            </FormGroup>
-          </Row>
-        </div>
-        <div className="signupButton"> 
-          <Button type="button" bsSize="large" bsStyle="primary">Sign Up</Button>
-        </div>
-      </Form>
+      <div>
+        <div className="errorMessage">{{this.state.errorMessage}}</div>
+        <Form inline className="signupForm" onSubmit={this.createNewAccount}>
+          <div className="nameFields">
+            <Row>
+              <FormGroup>
+                <FormControl 
+                  type="text" 
+                  name="firstName" 
+                  placeholder="First Name"
+                  value={this.state.firstName}
+                  onChange={this.handleChange} 
+                  />
+                <FormControl 
+                  type="text" 
+                  name="lastName" 
+                  placeholder="Last Name"
+                  value={this.state.lastName}
+                  onChange={this.handleChange} 
+                  />
+              </FormGroup>
+            </Row>
+          </div>
+          <div className="singleRows">
+            <Row>
+              <FormGroup>
+                <FormControl 
+                  type="text" 
+                  name="email" 
+                  placeholder="Email"
+                  value={this.state.email}
+                  onChange={this.handleChange} 
+                  />
+              </FormGroup>
+            </Row>
+            <Row>
+              <FormGroup>
+                <FormControl 
+                  type="password" 
+                  name="password" 
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange} 
+                  />
+              </FormGroup>
+            </Row>
+            <Row>
+              <FormGroup>
+                <FormControl 
+                  type="text" 
+                  name="location" 
+                  placeholder="City, State"
+                  value={this.state.location}
+                  onChange={this.handleChange}  
+                  />
+              </FormGroup>
+            </Row>
+          </div>
+          <div className="signupButton"> 
+            <Button type="button" bsSize="large" bsStyle="primary">Sign Up</Button>
+          </div>
+        </Form>
+      </div>
     )
   }
 }
